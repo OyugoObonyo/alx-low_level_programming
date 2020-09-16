156ffdc801f76729b5fa00c9b0f5a60357e00724
OyugoObonyo <nyikal1999@gmail.com>
2020-09-16 18:31:24 +0000

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+char *s = "\"Programming is like building a multilingual puzzle";
+
+puts(s);
+
+return (0);
+}
