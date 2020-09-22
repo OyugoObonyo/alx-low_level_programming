104092a90451e3159e2e7c6a1ba1813279bed5f5
OyugoObonyo <nyikal1999@gmail.com>
2020-09-22 04:28:25 +0000

    Header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,7 +2,15 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
-
+void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int print_sign(int n);
+int print_sign(int n);
+int print_sign(int n);
+void times_table(void);
+int add(int, int);
 
 #endif
