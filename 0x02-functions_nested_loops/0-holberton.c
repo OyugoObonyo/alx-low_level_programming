924a98267d09228e6a5ae810acf8742477759008
OyugoObonyo <nyikal1999@gmail.com>
2020-09-22 05:27:16 +0000

    Print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,7 +4,6 @@
  * main - Printing the word Holberton
  *
  * Return: Always 0 (success)
- *
  */
 
 int main(void)
@@ -12,7 +11,7 @@ int main(void)
 char *school_name = "Holberton";
 int letters = 0;
 
-for (letters = 0; letters <= 9; letters++)
+for (letters = 0; letters <= 8; letters++)
 {
 _putchar(school_name[letters]);
 }
