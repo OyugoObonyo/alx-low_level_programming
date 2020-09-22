f4fc7980990dde8fe093cab8383539881984a39d
OyugoObonyo <nyikal1999@gmail.com>
2020-09-22 05:16:22 +0000

    Print in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase with new line
+ *
+ */
+
+void print_alphabet(void)
+{
+char alphabets;
+
+for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+{
+_putchar(alphabets);
+}
+_putchar('\n');
+
+}
