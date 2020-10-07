861a71b49748380da959b6a2f45ec6eaae2aca68
OyugoObonyo <nyikal1999@gmail.com>
2020-10-07 18:59:50 +0000

    Prints string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,14 +10,14 @@ int _strlen_recursion(char *s)
 {
 
 if (!*s)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}	
-	}
+{
+return (0);
+}
+else
+{
+return (1 + _strlen_recursion(s + 1));
+}
+}
 
 
 
