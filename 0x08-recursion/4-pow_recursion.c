800098c40009075c237687a429a5714fef0453b8
OyugoObonyo <nyikal1999@gmail.com>
2020-10-07 20:25:22 +0000

    Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *_pow_recursion- function that gives power of x by y
+ *@x:1st parameter
+ *@y:2nd parameter
+ *Return: power of x
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0) 
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1)); 
+ 			}
+	}
