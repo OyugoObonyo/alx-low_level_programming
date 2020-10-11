7e923af830e028397251afe0ef676ca58cba635d
OyugoObonyo <nyikal1999@gmail.com>
2020-10-11 13:28:09 +0000

    Number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *@argc:1st parameter
+ *@argv:2nd parameter
+ *Return:0 if succesful
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
