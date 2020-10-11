7a5f1ce5351037ee044e7e22c0af4d00c97986fa
OyugoObonyo <nyikal1999@gmail.com>
2020-10-11 13:16:45 +0000

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main-entry point
+ *@argc:first parameter
+ *@argv:second parameter
+ *Return:0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
