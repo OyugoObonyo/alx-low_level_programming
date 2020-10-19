d582d4b6c4b54d94dfbe355149087b4474b817fc
OyugoObonyo <nyikal1999@gmail.com>
2020-10-19 17:59:52 +0000

    New structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog-new data structure
+*@name:dog's name
+*@age:age of the dog
+*@owner: owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
