3440c54979d1f55d5c7670d43aab86883b8642e0
OyugoObonyo <nyikal1999@gmail.com>
2020-10-21 09:16:08 +0000

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+*print_name-Prints a name
+*@name:Parameter 1
+*@f:parameter 2
+*Return:Nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
