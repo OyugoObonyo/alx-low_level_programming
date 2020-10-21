fe3f4262c783044d141551c42ec3f677661817da
OyugoObonyo <nyikal1999@gmail.com>
2020-10-21 20:08:55 +0000

    Main file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,43 +1,32 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "3-calc.h"
 
 /**
- * main - returns result of calculation
- * @argc: first parameter
- * @argv: second parameter
- *solution credit from Ian
- * Description: returns result of calculation
- * Return: Always(0) Success
+ * main - prints the result between two operators.
+ * @argc: number of arguments received.
+ * @argv: array of arguments received.
+ *
+ * Return: 0 (success).
  */
+
 int main(int argc, char *argv[])
 {
-	int calc, num1, num2;
-	char operator[1];
+	int num1 = 0;
+	int num2 = 0;
+	int res;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	num1 = atoi(argv[1]);
-	strcpy(operator, *(argv + 2));
 	num2 = atoi(argv[3]);
 
+	res = get_op_func(argv[2])(num1, num2);
 
-	if (get_op_func(operator) == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	calc = (get_op_func(operator)(num1, num2));
-
-	printf("%d\n", calc);
+	printf("%d\n", res);
 
 	return (0);
 }
-
-
