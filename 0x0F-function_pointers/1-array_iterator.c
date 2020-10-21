98e5d2d4583781a8a8b6391348d335c5d1601334
OyugoObonyo <nyikal1999@gmail.com>
2020-10-21 13:23:24 +0000

    Action

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+*array_iterator-executes function on each element of the array
+*@array:Parameter 1
+*@size:Parameter 2
+*@action:Parameter 3
+*Return:Nothing
+*/
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	for(i=0;i<size;i++)
+
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
