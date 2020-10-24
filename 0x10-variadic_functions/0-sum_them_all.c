7bc8c4a9a7a16d5bba64cde7455ae0a0b0f5eb38
OyugoObonyo <nyikal1999@gmail.com>
2020-10-24 12:57:38 +0000

    Finds the sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all-Returns sum of all parameters
+ *@n:First Parameter
+ *Return:Sum of all parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list arg;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg, int);
+	}
+	va_end(arg);
+	return (sum);
+}
