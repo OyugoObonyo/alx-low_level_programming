a5171de4b1d1682e349d0c0d0c0ed62fae99120b
OyugoObonyo <nyikal1999@gmail.com>
2020-12-13 19:48:11 +0000

    Header File

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -12,9 +15,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
