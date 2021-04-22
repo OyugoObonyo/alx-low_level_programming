396cae98008c90e1a82697b566755e038d218c1a
OyugoObonyo <nyikal1999@gmail.com>
2021-04-22 20:35:53 +0300

    Implement print array function

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,33 @@
 #include "search_algos.h"
 
 /**
+ * print_array - prints the contents inside the array
+ * @array: the array to print
+ * @l: start point
+ * @r: end point
+ * Return: void
+ */
+void print_array(int *array, size_t l, size_t r)
+{
+	size_t i;
+
+	i = l;
+	if (i <= r)
+		printf("Searching in array: ");
+	while (l <= r)
+	{
+		printf("%u", array[l]);
+		if (l != r)
+			printf(", ");
+		++l;
+	}
+
+	if (i <= r)
+		printf("\n");
+}
+
+
+/**
  *binary_search - finds an element in an array using binary search
  *@array: array to be traversed
  *@size: size of the array
@@ -12,28 +39,23 @@ int binary_search(int *array, size_t size, int value)
 	size_t left = 0;
 	size_t right = size - 1;
 	int middle;
-	size_t i;
 
 	while (left <= right)
 	{
 		middle = (left + right) / 2;
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
-
-			if (value == array[middle])
-			{
-				return (middle);
-			}
-			else if (value < array[middle])
-			{
-				right = middle - 1;
-			}
-			else
-			{
-				left = middle + 1;
-			}
+		print_array(array, left, right);
+		if (value == array[middle])
+		{
+			return (middle);
 		}
-		return (-1);
+		else if (value < array[middle])
+		{
+			right = middle - 1;
+		}
+		else
+		{
+			left = middle + 1;
+		}
 	}
+	return (-1);
+}
