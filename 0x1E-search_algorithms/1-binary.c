3ea1273ff93a67526736bed8e0af805a5c1d0b13
OyugoObonyo <nyikal1999@gmail.com>
2021-04-22 20:20:51 +0300

    Fix: while loop condition

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 	int middle;
 	size_t i;
 
-	while (left < right)
+	while (left <= right)
 	{
 		middle = (left + right) / 2;
 		printf("Searching in array: ");
