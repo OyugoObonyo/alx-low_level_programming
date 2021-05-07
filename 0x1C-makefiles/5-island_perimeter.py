d7fd3f047291deb18e6f6e2447f90b7a190f545e
OyugoObonyo <nyikal1999@gmail.com>
2021-05-07 22:27:02 +0300

    Handle list with only zeros

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python4
 """
 5-island_perimeter
 """
@@ -6,13 +6,19 @@
 
 def island_perimeter(grid):
     """Finds perimeter of island provided as grid"""
+    gridWidth = len(grid)
+    gridLength = len(grid[0])
     island_count = 0
     perimeter = 0
     sides = 4
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                island_count += 1
-    perimeter = ((sides * island_count)/2) + 2
-    return int(perimeter)
+    if gridLength <= 100 and gridWidth <= 100:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    island_count += 1
+        if island_count == 0:
+            perimeter = 0
+            return perimeter
+        perimeter = ((sides * island_count)/2) + 2
+        return int(perimeter)
